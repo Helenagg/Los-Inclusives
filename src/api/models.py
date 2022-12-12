96b88e1779870727bf560fb8d58b1ce40037d783
Javier Murillo Romero <javim552@gmail.com>
2022-12-12 18:05:48 +0000

    Prueba de login

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    nombre = db.Column (db.String(80), unique=True, nullable=False)
+    apellidos = db.Column (db.String(80), unique=True, nullable=False)
+    telefono = db.Column (db.Integer, unique=True, nullable=True)
+    direccion = db.Column (db.String(80), unique=True, nullable=True)
+    is_parents = db.Column(db.Boolean(), unique=False, nullable=False) # Aquí decimos si es padre o no para decidir por una ruta padres o profesor
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +19,9 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "nombre": self.nombre,
+            "apellidos": self.apellidos,
+            "telefono" : self.telefono,
+            "direccion" : self.direccion
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,18 +1,63 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, jsonify, url_for, Blueprint, json
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
 
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = request.data
+    data = json.loads (data)
+    user = User(email = data["email"], 
+    password = data["password"],
+    is_parents = data["is_parents"],
+    nombre = data["nombre"],
+    apellidos = data["apellidos"],
+    telefono = data["telefono"],
+    direccion = data["direccion"])
+    db.session.add(user)
+    db.session.commit()
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "message": "Usuario Creado"
     }
 
+    return jsonify(response_body)
+
+@api.route('/login', methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    is_parents = request.json.get ("is_parents", None) # Meter si es padre o profesor
+    user = User.query.filter_by(email=email, password=password, is_parents= is_parents).first()
+   
+    if user == None:
+        return jsonify ({"msg": "Email o contraseña incorrecto"}),401
+    access_token = create_access_token(identity=user.email)
+        
+    response_body = {
+        "message" : "Accediendo a login",
+        "token" : access_token
+    }
+    return jsonify(response_body),200
+
+
+@api.route("/private", methods=["GET"])
+@jwt_required()
+def private():
+    # Access the identity of the current user with get_jwt_identity
+    response_body = {
+        "message": "Accediendo a privada",
+        "correcto": "true",
+        "user": get_jwt_identity()
+    }
+    
     return jsonify(response_body), 200
