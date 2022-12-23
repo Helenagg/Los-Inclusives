01368bd556ef4c88e73eef854f48fb8e42aa2b05
Javier Murillo Romero <javim552@gmail.com>
2022-12-23 12:00:57 +0000

    creacion de vista niños y card

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,25 +1,33 @@
 import React, { useContext,useState, useEffect } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
+import background from "../../img/manos.png";
+
 import "../../styles/home.css";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 	const navigate = useNavigate();
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [isChecked, setIsChecked] = useState(false);
 	const [error, setError] = useState("");
+
+	const handleOnChange = () => {
+        setIsChecked(!isChecked);
+      };
   
-	const Login = () => {
+	const login = () => {
 	  console.log(email, password);
 	  var myHeaders = new Headers();
 	  myHeaders.append("Content-Type", "application/json");
   
-	//   var raw = JSON.stringify({
-	// 	email: email,
-	// 	password: password,
-	//   });
+	  var raw = JSON.stringify({
+		"email": email,
+		"password": password,
+		"is_parents": isChecked
+	  });
   
 	  var requestOptions = {
 		method: "POST",
@@ -29,7 +37,7 @@ export const Home = () => {
 	  };
   
 	  fetch(
-		"https://3001-helenagg-losinclusives-uxilung57a3.ws-eu78.gitpod.io/api/login",
+		"https://3001-helenagg-losinclusives-ome5w3o6m5y.ws-eu80.gitpod.io/api/login",
 		requestOptions
 	  )
 		.then((response) => response.json())
@@ -45,25 +53,25 @@ export const Home = () => {
 	};
   
 	return (
-	  <div className="text-center mt-5">
-		<h1>VAMOS A ENTRAR</h1>
-  
-		<div>
-		  <label>E-mail:</label>
-		  <input onChange={(event) => setEmail(event.target.value)}></input>
-		  <label className="m-2">Contraseña: </label>
-		  <input onChange={(event) => setPassword(event.target.value)}></input>
-		</div>
-  
-		<div>
-		  <button onClick={Login}>Login</button>
-		  {error && (
-			<div className="alert alert-danger" role="alert">
-			  {error}
+		<>
+			<div className="container text-center mt-5" style={{backgroundImage: `url(${background})`}}>
+				<div className="row justify-content-md-center"> 
+					<div className="col-3 border bg-white">
+						<h1 className="text-success">LOGIN</h1>
+						<input className="form-check-input m-2" type="checkbox" checked={isChecked} onChange={handleOnChange}></input>
+                        <label className="form-check-label">Padre</label>
+						<input className="form-control mb-3 border border-success" placeholder="Email" onChange={(event) => setEmail(event.target.value)}></input>
+						<input className="form-control mb-3 border border-success" placeholder="Contraseña" onChange={(event) => setPassword(event.target.value)}></input>
+						<button className="btn btn-outline-success m-3" onClick={login}>Login</button>
+						<button className="btn btn-outline-success"><Link to={"/signup"} style={{textDecoration: 'none', color: 'green'}}>Registro</Link></button>
+						{error && (
+							<div className="alert alert-danger" role="alert">
+							{error}
+							</div>
+						)}
+					</div>
+				</div>
 			</div>
-		  )}
-		</div>
-	  </div>
+		</>
 	);
   };
-  
