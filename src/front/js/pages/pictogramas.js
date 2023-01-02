6089283f0c619679b4cd28217294efc74367d8d8
Helenagg <hgonzalezg18@gmail.com>
2023-01-02 11:08:48 +0000

    botón busqueda creado

--- src/front/js/pages/pictogramas.js
+++ src/front/js/pages/pictogramas.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Pictogramas = () => {
 
+    const [buscar, setBuscar] = useState("");
+
+    const busqueda = () => {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+          
+          fetch("https://api.arasaac.org/api/pictograms/es/search/dientes", requestOptions)
+            .then(response => response.json())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+    }
+
     return (
-        <div>
-            <h1> Parte de pictogramas</h1>
-        </div>
+        <>
+            <div className="container text-center mt-5">
+                <div className="col-3">
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Buscar pictograma" aria-label="Search" onChange={(event) => setBuscar(event.target.value)}></input>
+                        <button className="btn btn-outline-success" type="submit" onClick={busqueda}>Buscar</button>
+                    </form>
+                    <p></p>
+                </div>
+            </div>
+        </>
     )
 }
