a09a11f996d3b8daad94e35977e5b78fd3d15e52
Helenagg <hgonzalezg18@gmail.com>
2023-01-09 20:25:13 +0000

    pdte error

--- src/front/js/pages/pictogramas.js
+++ src/front/js/pages/pictogramas.js
@@ -2,12 +2,13 @@ import { element } from "prop-types";
 import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { CardPictogramas } from "../component/cardPictogramas";
+import { Link } from "react-router-dom"
 import "../../styles/cardPictogramas.css";
 
 export const Pictogramas = (props) => {
   const [buscar, setBuscar] = useState("");
   const [result, setResult] = useState([]);
-  // const [url, setUrl] = useState("");
+  const [error, setError] = useState("");
 
     // realizamos búsqueda de todos los pictogramas 
   const busqueda = () => {
@@ -16,8 +17,16 @@ export const Pictogramas = (props) => {
     })
       .then((response) => response.json())
       .then((result) => setResult(result))
+      //   if (!result === null) {setResult(result)
+      // } 
+      // else {
+      //   setError(error)}})
+
+       // Crear alerta de error si la palabra no es encontrada
+       // Se ha creado un useState error línea 11
+       //Se muestra en el DIV de la línea 87-89 ahora comentada
       .catch((error) => console.log("error", error));
-    console.log(result);
+    
   };
 
    // enviamos a la tabla Pictogramas el pictograma seleccionado
@@ -39,7 +48,7 @@ export const Pictogramas = (props) => {
     };
 
     fetch("https://3001-helenagg-losinclusives-13ek3qbc41f.ws-eu81.gitpod.io/api/pictogramas", requestOptions)
-      .then(response => response.text())
+      .then(response => response.json())
       .then(result => console.log(result))
       .catch(error => console.log('error', error));
   }
@@ -48,32 +57,37 @@ export const Pictogramas = (props) => {
   return (
     <>
       <div className="container text-center mt-5">
-        <div className="col-3">
-          <div className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Buscar pictograma" aria-label="Search"
-              onChange={(event) => setBuscar(event.target.value)}
-            ></input>
-            <button className="btn btn-outline-success" onClick={busqueda}>
-              Buscar
-            </button>
+        <div className="row">
+          <div className="col-3">
+            <div className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Buscar pictograma" aria-label="Search"
+                onChange={(event) => setBuscar(event.target.value)}
+              ></input>
+              <button className="btn btn-outline-success" onClick={busqueda}>
+                Buscar
+              </button>
+            </div>
+            {result.map((element) => {
+              return (
+                  <CardPictogramas
+                  imagen={"https://static.arasaac.org/pictograms/"+element._id+"/"+element._id+"_300.png"}
+                  descripcion={
+                    <>
+                      <p><button className="btn btn-outline-success" onClick={()=>seleccionar("https://static.arasaac.org/pictograms/"+element._id+"/"+element._id+"_300.png")}><Link to={"/parents"}>Seleccionar</Link></button></p>
+                      {element.keywords?.map((keyword) => {
+                        return (                   
+                            <p>{keyword.keyword}</p>
+                        );
+                      })}
+                    </>
+                  }
+                />
+              );
+            })}
           </div>
-          {result.map((element) => {
-            return (
-                <CardPictogramas
-                imagen={"https://static.arasaac.org/pictograms/"+element._id+"/"+element._id+"_300.png"}
-                descripcion={
-                  <>
-                    <p><button className="btn btn-outline-success" onClick={()=>seleccionar("https://static.arasaac.org/pictograms/"+element._id+"/"+element._id+"_300.png")}>Seleccionar</button></p>
-                    {element.keywords?.map((keyword) => {
-                      return (                   
-                          <p>{keyword.keyword}</p>
-                      );
-                    })}
-                  </>
-                }
-              />
-            );
-          })}
+          {/* {error && <div class="alert alert-danger" role="alert">
+				  {error}
+				</div>} */}
         </div>
       </div>
     </>
