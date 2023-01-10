834bd020871d0b9b60de4ed37ed83ef908bcc47a
Helenagg <hgonzalezg18@gmail.com>
2023-01-10 10:37:27 +0000

    footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,9 +2,12 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
+		<p className="text-start offset-1">
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
 			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+		</p>
+		<p className="text-end">
+			Thanks to <a href="https://arasaac.org/" >© ARASAAC - Gobierno de Aragón, 2023</a>
 		</p>
 	</footer>
 );
